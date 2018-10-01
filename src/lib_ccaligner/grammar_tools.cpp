d690d06fc3c431a5031def0c4bedfa213c2aa32f
Alexandru Văleanu <valeanu_alex@yahoo.com>
2018-10-01 08:33:57 +0300

    Rename --model parameter with --model_dir in g2p-seq2seq call

--- src/lib_ccaligner/grammar_tools.cpp
+++ src/lib_ccaligner/grammar_tools.cpp
@@ -184,7 +184,7 @@ void GenerateDict(bool generateQuickDict) // Generate dictionary from tensor flo
     {
         INFO << "Creating the Dictionary, this might take some time depending "
             "on your TensorFlow configuration : tempFiles/dict/complete.dict";
-        int rv = systemGetStatus("g2p-seq2seq --decode tempFiles/vocab/complete.vocab --model g2p-seq2seq-cmudict/ > tempFiles/dict/complete.dict");
+        int rv = systemGetStatus("g2p-seq2seq --decode tempFiles/vocab/complete.vocab --model_dir g2p-seq2seq-cmudict/ > tempFiles/dict/complete.dict");
 
         if (rv != 0)
         {
